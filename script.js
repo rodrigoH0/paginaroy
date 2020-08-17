17a2ce23bfe5ab4bdad57ad93c31093701f82c25
rodrigoH0 <69820969+rodrigoH0@users.noreply.github.com>
2020-08-17 16:01:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//instrucciones en javascript este comentario
+
+alert("hola mundo web");
+
+//declarando una variable string ="3I"; en c#
+//javascript no se le pone el tipo de dato
+//utilizando let y var para variables, const(constantes)
+
+
+let grupo= "3I";
+//console.writeline() es lo mismo que console.log
+
+console.log(grupo);
+//existen 3 formas de crear variables
+let lenguaje=4;
+
+console.log("numero de lenguajes aprendidos"+
+lenguaje);
+
+//una forma de decir variables.
+//console.log(nombre);
+var num1, num2 ;
+var nombre;
+var edad;
+
+edad=27;
+nombre="diana";
+console.log(nombre);
+console.log(edad);
+console.log(num1);
+
+var estatura;
